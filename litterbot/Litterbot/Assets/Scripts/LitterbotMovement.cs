985a6145863928db4886f624067acc42515a6548
MrFirekiller <mrfirekiller101@gmail.com>
2022-07-12 17:20:42 +0100

    main robot and basic movement

--- litterbot/Litterbot/Assets/Scripts/LitterbotMovement.cs
+++ litterbot/Litterbot/Assets/Scripts/LitterbotMovement.cs
@@ -3,26 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LitterbotMovement : MonoBehaviour {
+    Rigidbody bot;
+    Vector3 vel;
+    float rotY;
+    float rotZ;
+
+    int counter = 0;
+    int loops = 0;
+
     // Start is called before the first frame update
     void Start() {
+        bot = GetComponent<Rigidbody>();
+        vel = new Vector3(0, 0, 0.25f);
+        rotY = bot.transform.eulerAngles.y;
+        rotZ = bot.transform.eulerAngles.z;
     }
 
     // Update is called once per frame
+
+    // TODO use delta time
     void Update() {
-        if (Input.GetKeyDown("w")) {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
+        counter++;
+
+        bot.transform.eulerAngles = new Vector3(
+            bot.transform.eulerAngles.x,
+            rotY,
+            rotZ
+        );
+
+        if (counter >= 10) {
+            bot.velocity = vel;
+            counter = 0;
+            loops++;
         }
-        if (Input.GetKey("a")) {
-            GetComponent<Rigidbody>().velocity = new Vector3(-5, 0, 0);
+
+        if (loops >= 340) {
+            vel = new Vector3(0.25f, 0, 0);
+            rotZ += 90;
+            loops = 0;
         }
-        if (Input.GetKey("s")) {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -5);
-        }
-        if (Input.GetKey("d")) {
-            GetComponent<Rigidbody>().velocity = new Vector3(5, 0, 0);
-        }
-        if (Input.GetKey("space")) {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 5, 0);
-        }
+
+        Debug.Log(loops);
+    }
+
+    void Turn(bool rightOrLeft) {
+
+    }
+
+    void Turn(float angX, float angY, float angZ) {
+
     }
 }
