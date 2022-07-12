eb6b8b9b4b9b7f4b55b447590654f13f3c836f3d
MrFirekiller <mrfirekiller101@gmail.com>
2022-07-12 19:15:27 +0100

    something i cba to put a message

--- litterbot/Litterbot/Assets/Scripts/LitterbotMovement.cs
+++ litterbot/Litterbot/Assets/Scripts/LitterbotMovement.cs
@@ -1,56 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class LitterbotMovement : MonoBehaviour {
-    Rigidbody bot;
-    Vector3 vel;
-    float rotY;
-    float rotZ;
+    //bool onReturn = false;
+    //bool[] transformSet = {false, false}
+    //bool rotComplete = true;
 
-    int counter = 0;
-    int loops = 0;
+    Vector3 start;
+    Vector3 destination;
+    bool endReached = false;
 
-    // Start is called before the first frame update
-    void Start() {
-        bot = GetComponent<Rigidbody>();
-        vel = new Vector3(0, 0, 0.25f);
-        rotY = bot.transform.eulerAngles.y;
-        rotZ = bot.transform.eulerAngles.z;
+    [SerializeField] Transform destinationObject;
+    [SerializeField] Transform[] junctionLocations;
+    [SerializeField] float[] junctionTurnAngles;
+    bool[] junctionsPassed;
+
+    void Awake() {
+        start = transform.position;
+        destination = destinationObject.position;
+        if (junctionLocations.Length != 0) {
+            junctionsPassed = new bool[junctionLocations.Length];
+            foreach (bool passed in junctionsPassed) {
+                passed = false;
+            }
+        }
     }
 
     // Update is called once per frame
-
-    // TODO use delta time
     void Update() {
-        counter++;
+        transform.position += Vector3.forward/4 * Time.deltaTime;
 
-        bot.transform.eulerAngles = new Vector3(
-            bot.transform.eulerAngles.x,
-            rotY,
-            rotZ
-        );
+        if (junctionLocations.Length != 0) {
+            foreach (Transform junctionLocation in junctionLocations) {
+                if (transform.position + new Vector3(0.1f, 0.1f, 0.1f) >= junctionLocation.position && transform.position - new Vector3(0.1f, 0.1f, 0.1f) <= junctionLocation.position) {
 
-        if (counter >= 10) {
-            bot.velocity = vel;
-            counter = 0;
-            loops++;
+                }                
+            }
         }
-
-        if (loops >= 340) {
-            vel = new Vector3(0.25f, 0, 0);
-            rotZ += 90;
-            loops = 0;
-        }
-
-        Debug.Log(loops);
     }
 
-    void Turn(bool rightOrLeft) {
-
-    }
-
-    void Turn(float angX, float angY, float angZ) {
-
+    void Turn(float ang) {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, ang, 0),  0);
     }
 }
