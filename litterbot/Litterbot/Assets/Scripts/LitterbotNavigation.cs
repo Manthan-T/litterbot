ffe3ecf8d062f13f4332ba0181c9eea8c8ceb301
MrFirekiller <mrfirekiller101@gmail.com>
2022-07-15 18:00:34 +0100

    lidar

--- litterbot/Litterbot/Assets/Scripts/LitterbotNavigation.cs
+++ litterbot/Litterbot/Assets/Scripts/LitterbotNavigation.cs
@@ -1,4 +1,5 @@
 // Import various libraries (boilerplate)
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,6 +24,8 @@ public class LitterbotNavigation : MonoBehaviour {
     // This is a Litterbot's navigation AI/controller
     NavMeshAgent nvm;
 
+    List<GameObject> rubbishObjectQueue = new List<GameObject>();
+
     // Awake is called before the first frame update. Unlike "Start", it is called regardless of whether the script is disabled.
     void Awake() {
         // Get the "NavMeshAgent" component the we gave the Litterbot during its development in Unity, and assign it to "nvm" so we
@@ -36,7 +39,7 @@ public class LitterbotNavigation : MonoBehaviour {
         nvm.destination = destinations[currentDestination].position;
         
         // If the Litterbot reaches its destination...
-        if (Vector3.Distance(nvm.destination, transform.position) <= 0.35) {
+        if (Vector3.Distance(nvm.destination, transform.position) <= 0.34) {
             // ...check if either:
             // a) the Litterbot is going forwards (not backwards in the code) and the final destination has been reached
             if (!backwards && currentDestination == destinations.Length - 1)
@@ -58,12 +61,59 @@ public class LitterbotNavigation : MonoBehaviour {
                 currentDestination--;
         
             /*
-             *
-             * Another way to understand this if-else statement is that if the Litterbot is going forwards, then the array is
-             * in the correct, forwards order. If it is going backwards, then the destinations in the array are in reverse order,
-             * And the else part therefore works backwards.
-             *
-             */
+            *
+            * Another way to understand this if-else statement is that if the Litterbot is going forwards, then the array is
+            * in the correct, forwards order. If it is going backwards, then the destinations in the array are in reverse order,
+            * And the else part therefore works backwards.
+            *
+            */
         }
+        
+        if (!(rubbishObjectQueue.Count == 0)) {
+            nvm.destination = rubbishObjectQueue[0].transform.position;
+
+            if (Vector3.Distance(nvm.destination, transform.position) <= 0.34) {
+                Destroy(rubbishObjectQueue[0]);
+                Debug.Log(rubbishObjectQueue[0].transform.position);
+                rubbishObjectQueue.RemoveAt(0);
+            }
+        }
+
+        for (int degree = 0; degree < 360; degree++) {
+            Vector3 start = new Vector3(transform.position.x, transform.position.y - 0.33f, transform.position.z);
+            var dir = Quaternion.Euler(0, degree, 0) * transform.right;
+
+            RaycastHit hit;
+            if (Physics.Raycast(start, dir * 2, out hit, (dir * 2).magnitude)) {
+                Vector3 check = new Vector3(transform.position.x, transform.position.y - 0.1225f, transform.position.z);
+                RaycastHit hit2;
+
+                if (!Physics.Raycast(check, dir * 2, out hit2, (dir * 2).magnitude)) {
+                    if (!ObjectPartOfTown(hit.colliderInstanceID)) {
+                            Debug.DrawRay(start, dir * hit.distance, Color.blue);
+                            if (!rubbishObjectQueue.Contains(hit.collider.gameObject)) {
+                                rubbishObjectQueue.Add(hit.collider.gameObject);
+                            }
+                    } else {
+                        Debug.DrawRay(start, dir * 2, Color.red);
+                    }
+                } else {
+                    Debug.DrawRay(start, dir * hit.distance, Color.yellow);
+                }
+            } else {
+                Debug.DrawRay(start, dir * 2, Color.red);
+            }
+        }
+    }
+    
+    public static bool ObjectPartOfTown(int iid) {
+        UnityEngine.Object obj = (UnityEngine.Object) typeof (UnityEngine.Object)
+                    .GetMethod("FindObjectFromInstanceID", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
+                    .Invoke(null, new object[] { iid });
+        Transform x = GameObject.Find(obj.name).transform.parent;
+        if (x != null)
+            return String.Equals(x.ToString(), "town (UnityEngine.Transform)");
+        else
+            return false;
     }
 }
