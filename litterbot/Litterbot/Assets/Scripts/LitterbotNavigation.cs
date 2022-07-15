57351cd00971daebb60706d05b672a220c79ae28
MrFirekiller <mrfirekiller101@gmail.com>
2022-07-15 21:16:01 +0100

    Proper nav

--- litterbot/Litterbot/Assets/Scripts/LitterbotNavigation.cs
+++ litterbot/Litterbot/Assets/Scripts/LitterbotNavigation.cs
@@ -1,5 +1,7 @@
-// Import various libraries (boilerplate)
+// Import the System library for string comparisons
 using System;
+
+// Import various libraries (boilerplate)
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,6 +26,7 @@ public class LitterbotNavigation : MonoBehaviour {
     // This is a Litterbot's navigation AI/controller
     NavMeshAgent nvm;
 
+    // This is the queue of all rubbish a Litterbot has detected
     List<GameObject> rubbishObjectQueue = new List<GameObject>();
 
     // Awake is called before the first frame update. Unlike "Start", it is called regardless of whether the script is disabled.
@@ -39,7 +42,7 @@ public class LitterbotNavigation : MonoBehaviour {
         nvm.destination = destinations[currentDestination].position;
         
         // If the Litterbot reaches its destination...
-        if (Vector3.Distance(nvm.destination, transform.position) <= 0.34) {
+        if (Vector3.Distance(nvm.destination, transform.position) <= 0.36) {
             // ...check if either:
             // a) the Litterbot is going forwards (not backwards in the code) and the final destination has been reached
             if (!backwards && currentDestination == destinations.Length - 1)
@@ -69,50 +72,82 @@ public class LitterbotNavigation : MonoBehaviour {
             */
         }
         
+        // If there is rubbish to be collected...
         if (!(rubbishObjectQueue.Count == 0)) {
-            nvm.destination = rubbishObjectQueue[0].transform.position;
+            // And if the object is still there (may have been picked up by another litterbot)...
+            if (rubbishObjectQueue[0] != null) {
+                // Set the new destination to the location of the rubbish.
+                nvm.destination = rubbishObjectQueue[0].transform.position;
 
-            if (Vector3.Distance(nvm.destination, transform.position) <= 0.34) {
-                Destroy(rubbishObjectQueue[0]);
-                Debug.Log(rubbishObjectQueue[0].transform.position);
+                // If the litterbot reaches the rubbish it is set on...
+                if (Vector3.Distance(rubbishObjectQueue[0].transform.position, transform.position) <= 0.36) {
+                    // Suck it up (remove it from the scene) and remove it from the list of rubbish to collect.
+                    Destroy(rubbishObjectQueue[0]);
+                    rubbishObjectQueue.RemoveAt(0);
+                }
+            
+            // Otherwise remove the non-existent object from the list of objects to pick up.
+            } else {
                 rubbishObjectQueue.RemoveAt(0);
             }
         }
 
+        // For every degree in a circle (in otherwords make a circle)...
         for (int degree = 0; degree < 360; degree++) {
-            Vector3 start = new Vector3(transform.position.x, transform.position.y - 0.33f, transform.position.z);
+            // Set the start of the ray and the direction it should go...
+            Vector3 start = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             var dir = Quaternion.Euler(0, degree, 0) * transform.right;
 
+            // Prepare for a potential hit and cast out the ray. If a hit is detected...
             RaycastHit hit;
             if (Physics.Raycast(start, dir * 2, out hit, (dir * 2).magnitude)) {
-                Vector3 check = new Vector3(transform.position.x, transform.position.y - 0.1225f, transform.position.z);
+                // Prepare for a second ray to be cast.
+                Vector3 check = new Vector3(transform.position.x, transform.position.y + 0.2075f, transform.position.z);
                 RaycastHit hit2;
 
+                // Check if the ray hits the object too. This ray is cast at a higher height, and if there is a hit, it
+                // means that the object is too tall to pass underneath the robot's vacuum, (eg, a car), and should be ignored.
+                // If the object is not too tall...
                 if (!Physics.Raycast(check, dir * 2, out hit2, (dir * 2).magnitude)) {
+                    // And the object is not a part of the town (ie, a sidewalk or something)
                     if (!ObjectPartOfTown(hit.colliderInstanceID)) {
+                            // Draw a blue ray for debugging purposes.
                             Debug.DrawRay(start, dir * hit.distance, Color.blue);
+
+                            // Add the ray to the queue or rubbish to pick up if it isn't already there.
                             if (!rubbishObjectQueue.Contains(hit.collider.gameObject)) {
                                 rubbishObjectQueue.Add(hit.collider.gameObject);
                             }
+                    // If the object is a part of the town, draw a red ray for debugging purposes.
                     } else {
                         Debug.DrawRay(start, dir * 2, Color.red);
                     }
+                // If the object cannot be picked up, draw a yellow ray for debugging purposes.
                 } else {
                     Debug.DrawRay(start, dir * hit.distance, Color.yellow);
                 }
+            // If no object cannot be picked up, draw a red ray for debugging purposes.
             } else {
                 Debug.DrawRay(start, dir * 2, Color.red);
             }
         }
     }
     
-    public static bool ObjectPartOfTown(int iid) {
+    // Finds out whether an object is a part of the town (and should be ignored) or not, given its ID.
+    public static bool ObjectPartOfTown(int id) {
+        // Make a Unity object with the flags below. We need to cast from a modified object to just an object.
         UnityEngine.Object obj = (UnityEngine.Object) typeof (UnityEngine.Object)
                     .GetMethod("FindObjectFromInstanceID", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
-                    .Invoke(null, new object[] { iid });
-        Transform x = GameObject.Find(obj.name).transform.parent;
-        if (x != null)
-            return String.Equals(x.ToString(), "town (UnityEngine.Transform)");
+                    .Invoke(null, new object[] { id });
+
+        // Get the parent of the object.
+        Transform parent = GameObject.Find(obj.name).transform.parent;
+
+        // If the object has a parent (otherwise parent is null).
+        if (parent != null)
+            // Return whether or not the parent is called town.
+            return String.Equals(parent.ToString(), "town (UnityEngine.Transform)");
+        // Otherwise return false.
         else
             return false;
     }
