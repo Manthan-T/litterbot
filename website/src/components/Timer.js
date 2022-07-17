3895cd09d6a49ed71153041e012fbbf6b96d56ba
MrFirekiller <mrfirekiller101@gmail.com>
2022-07-17 12:44:35 +0100

    more timer failure

--- website/src/components/Timer.js
+++ website/src/components/Timer.js
@@ -18,43 +18,45 @@ const Timer = ({ actionOnTimeUp, setInstructions, extra, timerNo, profile }) => 
 
     // The time left on the timer and a method to set it.
     const [timeLeft, setTimeLeft] = useState("5 minutes left")
-    let [intTimeLeft, setIntTimeLeft] = useState(300)
+    let intTimeLeft = 300
+
+    const onCount = () => {
+        // Set the time left as the time left - 1.
+        intTimeLeft--
+        setTimeLeft(formatTime(intTimeLeft))
+        //setTimeLeft(formatTime(intTimeLeft))
+
+        // If time is up, do the instructed action and reset the timer.
+        if (intTimeLeft === 0) {
+            actionOnTimeUp()
+            setInstructions("You ran out of time. Please leave this page and come back.")
+            if (timerNo === "1") {
+                localStorage.setItem("reportCooldown" + profile, false)
+            } else {
+                localStorage.removeItem("secsLeft" + profile + timerNo)
+            }
+        }
+    }
 
     // Starts the timer when the component is loaded.
     useEffect(() => {
         if (timerNo === "1") {
-            setIntTimeLeft(localStorage.getItem("secsLeft" + profile + timerNo) !== null ? localStorage.getItem("secsLeft" + profile + timerNo) - new Date().getSeconds() - localStorage.getItem("secsLeft" + profile + timerNo) : 300)
+            intTimeLeft = localStorage.getItem("secsLeft" + profile + timerNo) !== null ? localStorage.getItem("secsLeft" + profile + timerNo) - new Date().getSeconds() - localStorage.getItem("secsLeft" + profile + timerNo) : 300
         } else {
-            setIntTimeLeft(intTimeLeft = sessionStorage.getItem("secsLeft" + profile + timerNo) !== null ? sessionStorage.getItem("secsLeft" + profile + timerNo) - new Date().getSeconds() - sessionStorage.getItem("secsLeft" + profile + timerNo) : 300)
+            intTimeLeft = sessionStorage.getItem("secsLeft" + profile + timerNo) !== null ? sessionStorage.getItem("secsLeft" + profile + timerNo) - new Date().getSeconds() - sessionStorage.getItem("secsLeft" + profile + timerNo) : 300
         }
 
         if (intTimeLeft < 0) {
-            setIntTimeLeft(0)
+            intTimeLeft = 0
         }
+        
         formatTime(intTimeLeft)
 
         // A method that is executed every second.
-        let countdown = setInterval(() => {
-            // Set the time left as the time left - 1.
-            setIntTimeLeft(intTimeLeft - 1)
-            setTimeLeft(intTimeLeft)
-            console.log(intTimeLeft)
-            //setTimeLeft(formatTime(intTimeLeft))
-
-            // If time is up, do the instructed action and reset the timer.
-            if (intTimeLeft === 0) {
-                actionOnTimeUp()
-                clearInterval(countdown)
-                setInstructions("You ran out of time. Please leave this page and come back.")
-                if (timerNo === "1") {
-                    localStorage.setItem("reportCooldown" + profile, false)
-                } else {
-                    localStorage.removeItem("secsLeft" + profile + timerNo)
-                }
-            }
-        }, 1000)
+        const countdown = setInterval(onCount, 1000)
+        
         return () => clearInterval(countdown)
-    }, [actionOnTimeUp])
+    })
 
     useEffect(() => {
         window.addEventListener("beforeunload", () => {
@@ -63,7 +65,7 @@ const Timer = ({ actionOnTimeUp, setInstructions, extra, timerNo, profile }) => 
             } else {
                 sessionStorage.setItem("secsLeft" + profile + timerNo, intTimeLeft)
             }
-        });
+        })
         return () => {
             window.removeEventListener("beforeunload", () => {
                 if (timerNo === "1") {
@@ -71,9 +73,9 @@ const Timer = ({ actionOnTimeUp, setInstructions, extra, timerNo, profile }) => 
                 } else {
                     sessionStorage.setItem("secsLeft" + profile + timerNo, intTimeLeft)
                 }
-            });
-        };
-    }, []);
+            })
+        }
+    }, [])
 
     // Return the timer's text
     return (
